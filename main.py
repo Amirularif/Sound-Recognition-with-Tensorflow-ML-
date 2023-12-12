e1472539fa572bc600bd27eebfba7f75fba730e1
amirul <zainsyedahmad63@gmail.com>
2023-12-12 21:59:12 +0800

    second commit, change to MFCC to spectograms

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 # Import necessary libraries
 import tensorflow as tf
+from keras.src.utils import pad_sequences
+from sklearn.metrics import classification_report
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten, Conv1D, MaxPooling1D, LSTM
+from tensorflow.keras.layers import Dense, LSTM, Dropout
 from tensorflow.keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
 import librosa
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
-# Function to extract audio features using librosa
-def extract_features(audio_path):
+# Function to extract audio features using librosa and plot the spectrogram
+def extract_features_and_plot(audio_path, label):
     audio, _ = librosa.load(audio_path, sr=None)
-    mfccs = librosa.feature.mfcc(y=audio, sr=16000, n_mfcc=13)
-    return mfccs
+    spectrogram = librosa.feature.melspectrogram(y=audio, sr=16000, n_mels=128, hop_length=512)
+    log_spectrogram = librosa.power_to_db(spectrogram, ref=np.max)
+
+    # Plot the spectrogram
+    plt.figure(figsize=(10, 4))
+    librosa.display.specshow(log_spectrogram, sr=16000, x_axis='time', y_axis='mel')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title(f'Spectrogram for class: {label}')
+    plt.show()
+
+    return log_spectrogram
 
 # Data paths
 data_dir = "D:\surah_audio\wav"
@@ -24,14 +36,14 @@ labels = []
 for title in os.listdir(data_dir):
     title_path = os.path.join(data_dir, title)
     if os.path.isdir(title_path):
-        #print(f"Label: {title}")
+        print(f"Label: {title}")
         for audio_file in os.listdir(title_path):
             audio_path = os.path.join(title_path, audio_file)
             if audio_file.endswith(".wav"):
                 audio_files.append(audio_path)
                 labels.append(title)
-                duration = librosa.get_duration(filename=audio_path)
-                #print(f"  Audio File: {audio_file}, Duration: {duration:.2f} seconds")
+                duration = librosa.get_duration(path=audio_path)
+                print(f"  Audio File: {audio_file}, Duration: {duration:.2f} seconds")
 
 # Encode labels
 label_mapping = {label: idx for idx, label in enumerate(set(labels))}
@@ -43,11 +55,8 @@ X_train, X_test, y_train, y_test = train_test_split(audio_files, encoded_labels,
 # Function to load and preprocess audio data
 def load_and_preprocess(audio_paths, labels):
     features = []
-    max_length = 0
     for path, label in zip(audio_paths, labels):
-        feature = extract_features(path)
-        # Update max_length if the current feature length is greater
-        max_length = max(max_length, feature.shape[1])
+        feature = extract_features_and_plot(path,label)
         features.append((feature, label))
     return features
 
@@ -59,10 +68,14 @@ test_data = load_and_preprocess(X_test, y_test)
 X_train, y_train = zip(*train_data)
 X_test, y_test = zip(*test_data)
 
-# Pad sequences to the length of the longest sequence
-max_length = max(len(feature[0]) for feature in X_train)
-X_train = np.array([librosa.util.fix_length(x[0], size=max_length, axis=0) for x in X_train])
-X_test = np.array([librosa.util.fix_length(x[0], size=max_length, axis=0) for x in X_test])
+# Find the maximum shape of a spectrogram in your dataset
+max_shape = max(feature.shape for feature, _ in train_data)
+
+# Pad or truncate each spectrogram to the maximum shape
+# Pad or truncate each spectrogram to the maximum shape
+X_train = np.array([np.pad(feature, ((0, max_shape[0] - feature.shape[0]), (0, max_shape[1] - feature.shape[1])), mode='constant') for feature in X_train])
+X_test = np.array([np.pad(feature, ((0, max_shape[0] - feature.shape[0]), (0, max_shape[1] - feature.shape[1])), mode='constant') for feature in X_test])
+
 
 # Convert to NumPy arrays
 X_train, y_train = np.array(X_train), np.array(y_train)
@@ -72,17 +85,19 @@ X_test, y_test = np.array(X_test), np.array(y_test)
 X_train = np.expand_dims(X_train, axis=-1)
 X_test = np.expand_dims(X_test, axis=-1)
 
-# Build the neural network model with an LSTM layer
+# Build the neural network model with a stacked LSTM architecture
 model = Sequential()
-model.add(LSTM(64, input_shape=(None, X_train.shape[2])))
+model.add(LSTM(64, input_shape=(None, X_train.shape[2]), return_sequences=True))
+model.add(LSTM(64))  # You can add more LSTM layers if needed
 model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))  # Add dropout for regularization
 model.add(Dense(len(set(encoded_labels)), activation='softmax'))
 
 # Compile the model
-model.compile(optimizer=Adam(learning_rate=0.001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=Adam(learning_rate=0.0001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 # Train the model
-model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test), verbose=2)
+model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test), verbose=2)
 
 # Evaluate the model
 test_loss, test_accuracy = model.evaluate(X_test, y_test, verbose=2)
@@ -95,9 +110,21 @@ predictions = model.predict(X_test)
 predicted_labels = [list(label_mapping.keys())[list(label_mapping.values()).index(np.argmax(prediction))] for prediction in predictions]
 true_labels = [list(label_mapping.keys())[list(label_mapping.values()).index(label)] for label in y_test]
 
-# Print the predicted and true labels for each test sample
+# Print the predicted and true labels along with the corresponding audio file names and MFCC shapes
 for i in range(len(X_test)):
-    print(f"Audio File: {X_test[i]}, Predicted: {predicted_labels[i]}, True: {true_labels[i]}")
+    print(f"Audio File: {X_test[i]}, Predicted: {predicted_labels[i]}, True: {true_labels[i]}, MFCC Shape: {X_test[i].shape}")
 
-# Save the model for future use
-#model.save("/path/to/save/your/model")
+# Convert one-hot encoded predictions to labels
+predicted_labels = np.argmax(predictions, axis=1)
+
+# Plot the spectrograms for the first few examples in each class
+for label in set(labels):
+    class_examples = [audio_path for audio_path, class_label in zip(audio_files, labels) if class_label == label]
+    for example in class_examples[:3]:
+        extract_features_and_plot(example, label)
+
+
+# Print classification report
+print(classification_report(y_test, predicted_labels, target_names=label_mapping.keys()))
+model.summary()
+
